1d5ff188587b3476ffbe24fa663cb156a841e525
SlimjimmyMax <SlimjimmyMax@outlook.com>
2023-08-15 17:16:59 +0300

    5. Can you C me now?

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,13 +1,3 @@
 #!/usr/bin/python3
-
 def no_c(my_string):
-    new_string = ""
-    for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
-    return new_string
-
-if __name__ == "__main__":
-    print(no_c("Best School"))
-    print(no_c("Chicago"))
-    print(no_c("C is fun!"))
+    return ''.join(char for char in my_string if char.lower() != 'c')
