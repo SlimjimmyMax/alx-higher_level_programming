7aa349a80c889c9324ba7bd23d0bb4e4fb9477a2
SlimjimmyMax <slimjimmymax@outlook.com>
2023-10-25 10:11:03 +0000

    1. Safe printing of an integers list

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,15 +1,8 @@
 #!/usr/bin/python3
 
-def safe_print_list(my_list=[], x=0):
-    count = 0
-
+def safe_print_integer(value):
     try:
-        for i in range(x):
-            print(my_list[i], end="")
-            count += 1
-    except IndexError:
-        pass
-
-    print()
-
-    return count
+        print("{:d}".format(value))
+        return True
+    except (ValueError, TypeError):
+        return False
