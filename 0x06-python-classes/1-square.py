368b218478e5a8e99e0f143a8b353c4513812cc5
SlimjimmyMax <slimjimmymax@outlook.com>
2023-10-31 09:05:07 +0000

    1. Square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+class Square:
+    """Represents a square."""
+    def __init__(self, size):
+        """Initializes a new Square instance with a given size."""
+        self.__size = size
