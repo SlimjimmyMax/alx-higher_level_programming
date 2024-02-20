80cda587bdbc4b8e3db79a8d581282c72d6af948
SlimjimmyMax <slimjimmymax@outlook.com>
2024-02-20 20:42:35 +0000

    star wars

---
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const movieId = process.argv[2];
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${movieId}`;
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  try {
+    const movieData = JSON.parse(body);
+    console.log(movieData.title);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
+});
