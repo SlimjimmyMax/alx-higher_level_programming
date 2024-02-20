0178126137e730e7cb51566a10e50365f36b3e7c
SlimjimmyMax <slimjimmymax@outlook.com>
2024-02-20 21:00:30 +0000

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const request = require('request');
+
+const apiUrl = process.argv[2];
+
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+
+  if (response.statusCode !== 200) {
+    console.error(`Failed to fetch data. Status code: ${response.statusCode}`);
+    return;
+  }
+
+  try {
+    const todos = JSON.parse(body);
+    const completedTasksByUser = {};
+
+    todos.forEach(todo => {
+      if (todo.completed) {
+        if (completedTasksByUser[todo.userId]) {
+          completedTasksByUser[todo.userId]++;
+        } else {
+          completedTasksByUser[todo.userId] = 1;
+        }
+      }
+    });
+
+    console.log(completedTasksByUser);
+  } catch (parseError) {
+    console.error('Error parsing JSON:', parseError);
+  }
+});
